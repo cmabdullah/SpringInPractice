ddbfffc9564123b497d817395dd2835d0cd51c10
cmabdullah <cmabdullah21@gmail.com>
2017-11-25 20:12:14 +0600

    Spring working

---
+++ springtest/src/main/java/com/abdullah/spring/springtest/App.java
@@ -0,0 +1,19 @@
+package com.abdullah.spring.springtest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+    		Patient patient = (Patient)context.getBean("patient");
+        patient.speak();
+    		//System.out.println( "Hello World!" );
+        ((FileSystemXmlApplicationContext)context).close();
+    }
+}
---
+++ springtest/src/main/java/com/abdullah/spring/springtest/Patient.java
@@ -0,0 +1,7 @@
+package com.abdullah.spring.springtest;
+import java.util.*;
+public class Patient {
+	public void speak() {
+		System.out.println("Help me");
+	}
+}
