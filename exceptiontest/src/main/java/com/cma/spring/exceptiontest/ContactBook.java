e90efff08242c5b3eb830d2129809b8018064e1c
cmabdullah <cmabdullah21@gmail.com>
2017-12-22 20:40:50 +0600

    Spring Framework - When to use null to set a property , 18/b

--- exceptiontest/src/main/java/com/cma/spring/exceptiontest/ContactBook.java
+++ exceptiontest/src/main/java/com/cma/spring/exceptiontest/ContactBook.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 public class ContactBook {
 	private Map<String , EmargencyContact> contacts = new HashMap <String, EmargencyContact>();
-
+	private Patient patient;
+	public ContactBook(){
+		patient = new Patient(10, "Mainul");
+	}
 	public Map<String, EmargencyContact> getContacts() {
 		return contacts;
 	}
@@ -14,9 +17,18 @@ public class ContactBook {
 		this.contacts = contacts;
 	}
 
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
+		sb.append(patient == null ? "Patient is null\n": patient.toString()+"\n");
 		for (Map.Entry<String, EmargencyContact> contacts : contacts.entrySet() ){
 			sb.append(contacts.toString()+"\n");
 		}
